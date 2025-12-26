764c69aa6fae2ecd0f915596f0be69f05fb24249
Eyan <eyanmartucci11@gmail.com>
2025-12-26 16:49:08 -0700

    Input and Player Controller
    - Setup basic input system using InputActions and Unity Events.
    - Created a basic script called PlayerController and implemented horizontal movement.
    - Setup Player prefab.
    - Created EyanTestScene for my own debugging to avoid merge conflicts.

---
+++ WinterMelonJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;  // IMPORTANT: make sure you have this to work with input system
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+    
+    private Rigidbody2D body;
+    private float moveInput;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Handles changes to rigidbody velocity
+    private void FixedUpdate()
+    {
+        body.linearVelocity = new Vector2(moveInput * moveSpeed, body.linearVelocity.y);
+    }
+
+
+    // Handles movement input. Called by the InputAction component on the Move event.
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<float>();;
+    }
+
+
+}
