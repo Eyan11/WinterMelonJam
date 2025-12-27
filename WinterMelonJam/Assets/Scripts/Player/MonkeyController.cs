42be67a50c2ec592aebea6ca6e440c62e0b18cf7
Eyan <eyanmartucci11@gmail.com>
2025-12-27 12:54:53 -0700

    Monkey Progress
    - Added Rope prefab and tag.
    - Finished object detection system with monkey which interacts with the closest interactable with the rope, breakable, or non-breakable tag within the interact radius.

--- WinterMelonJam/Assets/Scripts/Player/MonkeyController.cs
+++ WinterMelonJam/Assets/Scripts/Player/MonkeyController.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;  // IMPORTANT: make sure you have this to work with input system
+using System.Collections.Generic;   // Need to use List data structure
 
 public class MonkeyController : MonoBehaviour
 {
-    [SerializeField] private float interactRadius;
-    [SerializeField] private float climbSpeed;
-    [SerializeField] private float throwSpeed;
+    [SerializeField] private float interactRadius = 0.5f;
+    [SerializeField] private float climbSpeed = 1f;
+    [SerializeField] private float throwSpeed= 1f;
     [SerializeField] private LayerMask interactableLayer;
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveSpeed = 2f;
     private Rigidbody2D body;
     private float moveInput;
 
@@ -25,8 +26,56 @@ public class MonkeyController : MonoBehaviour
         body.linearVelocity = new Vector2(moveInput * moveSpeed, body.linearVelocity.y);
     }
 
+    private void InteractWithRope(GameObject obj)
+    {
+        Debug.Log("InteractWithRope. Obj.name = " + obj.name);
+    }
 
-    // Handles movement input. Called by the InputAction component on the Move event.
+    private void InteractWithBox(GameObject obj)
+    {
+        Debug.Log("InteractWithBox. Obj.name = " + obj.name);
+    }
+
+    private void CheckForInteraction()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, interactRadius, interactableLayer);
+        List<Collider2D> collList = new List<Collider2D>(hits);
+
+        if (hits.Length <= 0)
+            return;
+        
+        // Use closest point of obj instead of its center/pivot point
+        collList.Sort((a, b) => {
+            float distA = ((Vector2)transform.position - a.ClosestPoint(transform.position)).sqrMagnitude;
+            float distB = ((Vector2)transform.position - b.ClosestPoint(transform.position)).sqrMagnitude;
+            return distA.CompareTo(distB);
+        });
+
+        GameObject hitObj = null;
+        foreach(Collider2D hit in collList)
+        {
+            hitObj = hit.transform.gameObject;
+
+            if(hitObj.CompareTag("Rope"))
+            {
+                InteractWithRope(hitObj);
+                return;
+            }
+            else if(hitObj.CompareTag("Breakable") || hitObj.CompareTag("Non-Breakable"))
+            {
+                InteractWithBox(hitObj);
+                return;
+            }
+        }
+    }
+
+
+
+
+    // *** Events *************************************************************
+
+
+    // Called by the InputAction component on the Move event.
     public void OnMove(InputAction.CallbackContext context)
     {
         if(!gameObject.activeInHierarchy)
@@ -36,19 +85,11 @@ public class MonkeyController : MonoBehaviour
     }
 
 
-    // Handles interact input. Called by the InputAction component on the Inter event.
+    // Called by the InputAction component on the Interact event.
     public void OnInteract(InputAction.CallbackContext context)
     {
-        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, 0f, Vector2.zero, interactRadius, interactableLayer);
-
-        if (hits.Length <= 0)
-            return;
-        
-        foreach(RaycastHit2D hit in hits)
-        {
-            
-        }
-
+        if(context.started && gameObject.activeInHierarchy)
+            CheckForInteraction();
     }
 
 
