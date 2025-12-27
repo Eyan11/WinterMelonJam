c7c1048bc4262d91578511d62462f1a7bded96eb
Ryley Hansen <rhanse28@asu.edu>
2025-12-27 02:57:09 -0700

    Added Prefabs
    I made prefabs for a key and a door, the key can be collected and the door will be open when its given key is collected.

---
+++ WinterMelonJam/Assets/Scripts/Interactables/KeyCollection.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class KeyCollection : MonoBehaviour
+{
+    public bool keyCollected;
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void Start()
+    {
+        keyCollected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D interactor)
+    {
+        if (interactor.CompareTag("Player"))
+        {
+            keyCollected = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
