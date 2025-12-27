edbd7ef481adb9208e454543a6dc8cf25bcdccec
Sharkiko <145565841+Sharkiko@users.noreply.github.com>
2025-12-27 01:26:44 -0700

    Turtle and Shell WIP
    Shell isn't fully implemented

---
+++ WinterMelonJam/Assets/Scripts/Interactables/Shell.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    // Shell max lifetime
+    [SerializeField] private float maxLifetime;
+    [SerializeField] private float maxDistanceSquared;
+    // Shell data
+    private BoxCollider2D collider;
+    private Rigidbody2D body;
+    private bool platformMode = false;
+    public float lifetime;
+    private Vector3 shellStartPos;
+
+    // Sets the life time to max
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        body = GetComponent<Rigidbody2D>();
+        lifetime = maxLifetime;
+        shellStartPos = transform.position;
+    }
+
+    // FixedUpdate is called once per frame to check on platform validity and when to break
+    private void FixedUpdate()
+    {
+        if (platformMode)
+        {
+            lifetime -= Time.fixedDeltaTime;
+            if (lifetime < 0) BreakShell();
+        }
+        else if ((shellStartPos-transform.position).sqrMagnitude >= maxDistanceSquared || body.linearVelocityX < 0.5f)
+        {
+            body.linearVelocity = Vector3.zero;
+            body.bodyType = RigidbodyType2D.Static;
+            collider.excludeLayers = 0;
+            platformMode = true;
+        }
+    }
+
+    // Plays shell breaking animation
+    public void BreakShell()
+    {
+        // TODO: josue do the shell break jig dance thing i'm sleepy
+        Destroy(gameObject);
+    }
+}
--- WinterMelonJam/Assets/Scripts/Player/TurtleController.cs
+++ WinterMelonJam/Assets/Scripts/Player/TurtleController.cs
@@ -1,9 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class TurtleController : MonoBehaviour
 {
+    // Settings for throw distance, speed, and what the hell the shell is
+    [SerializeField] private float turtleMoveSpeed;
+    [SerializeField] private float shellThrowSpeed;
+    [SerializeField] private GameObject shellTemplate;
+    // Shell data (shell shock!)
+    private GameObject shell;
+    private float shellThrowDirection = 1;
+    // Player info
+    private float moveInput;
+    private Rigidbody2D body;
 
+    // **********************************************
+    // UNITY ACTIONS
 
+    // Retrieves components on object activation
+    private void Awake()
+    {
+        body = transform.parent.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.linearVelocity = new Vector2(moveInput * turtleMoveSpeed, body.linearVelocity.y);
+    }
 
     // Called when entering this mask transformation
     public void OnMaskEnter()
@@ -15,5 +40,32 @@ public class TurtleController : MonoBehaviour
     public void OnMaskExit()
     {
         
+    }
+
+    // **********************************************
+    // EVENTS
+
+    // Uses InputAction to get the movement direction
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>().x;
+        if (moveInput != 0 && shell == null) shellThrowDirection = moveInput;
+    }
+
+    // Uses InputAction to track when the interaction key is used; when used, try to charge
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (shell != null && shell.IsDestroyed() == false) return;
+
+        shell = Instantiate(shellTemplate);
+        Rigidbody2D shellBody = shell.GetComponent<Rigidbody2D>();
+        if (shellBody == null)
+        {
+            Destroy(shell);
+            shell = null;
+            Debug.LogError("ERROR: Shell is missing a rigidbody! Clearing shell!");
+            return;
+        }
+        shellBody.linearVelocity = new Vector2(shellThrowDirection * shellThrowSpeed, 0);
     }
 }
