80b6fabf8019fc43fc6d56dcd52125414f44d7b8
Sharkiko <145565841+Sharkiko@users.noreply.github.com>
2025-12-30 03:31:07 -0700

    Scaled Sprites + Scalable Platform
    - Certain sprites have proper PPU units
    - Platform is now scalable. It may need adjustments due to the jagged edges

--- WinterMelonJam/Assets/Scripts/Sprites/CollisionTileParity.cs
+++ WinterMelonJam/Assets/Scripts/Sprites/CollisionTileParity.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 
+[ExecuteAlways]
 public class CollisionTileParity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Settings")]
+    [SerializeField] private bool activeInRuntime = false;
+    [SerializeField] private Vector3 transformScaleLock = Vector3.one;
+    //[SerializeField] private SpriteRenderer[] secondarySprites;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer baseSprite;
+    
+    private void Awake()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
+        baseSprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    ///  For editor only (unless activeInRuntime is true): locks the transform to a 
+    ///  specific scale to prevent accidental edits. Then, it rescales the box collider
+    ///  to be the same size as the base sprite. 
+    /// </summary>
+    private void Update()
     {
-        
+        if (Application.IsPlaying(gameObject) && !activeInRuntime) return; 
+
+        if (transform.localScale != transformScaleLock) transform.localScale = transformScaleLock;
+
+        boxCollider.size = baseSprite.size;
     }
 }
